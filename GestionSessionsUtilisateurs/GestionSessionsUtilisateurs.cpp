02847034f879451fc64421d9a0a587219443155a
hassan979-new <hagouram979@gmai.com>
2025-10-19 15:50:51 +0100

    tp5 complete

---
+++ GestionSessionsUtilisateurs/GestionSessionsUtilisateurs.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Utilisateur{
+    string nom;
+    string role;
+    static int nbUtilisateursConnectes;
+
+    public :
+        Utilisateur(string n,string r){
+            nom = n;
+            role = r;
+            nbUtilisateursConnectes++;
+        }
+
+        static int getNbUtilisateursConnectes(){
+            return nbUtilisateursConnectes;
+        }
+
+    ~Utilisateur(){
+        nbUtilisateursConnectes--;
+        cout<<"le nombre d'utilisateur rester"<<getNbUtilisateursConnectes()<<endl;    
+    }
+};
+
+int Utilisateur::nbUtilisateursConnectes=0;
+
+int main()
+{
+    Utilisateur u1("Hassan","visiteur");
+    cout<<"le nombre d'utilisateur actuel: "<<Utilisateur::getNbUtilisateursConnectes()<<endl;
+    Utilisateur u2("Taha","visiteur");
+    cout<<"le nombre d'utilisateur actuel: "<<Utilisateur::getNbUtilisateursConnectes()<<endl;
+    Utilisateur u3("Oussama","visiteur");
+    cout<<"le nombre d'utilisateur actuel: "<<Utilisateur::getNbUtilisateursConnectes()<<endl;
+    Utilisateur u4("Wissam","visiteur");
+    cout<<"le nombre d'utilisateur actuel: "<<Utilisateur::getNbUtilisateursConnectes()<<endl;
+    Utilisateur u5("Hamza","visiteur");
+    cout<<"le nombre d'utilisateur actuel: "<<Utilisateur::getNbUtilisateursConnectes()<<endl;
+
+    return 0;
+}
---
+++ tauxTVA_partage/tauxTVA_partage.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class Produit{
+    string nom;
+    float prixHT;
+    static float tauxTVA;
+
+    public :
+        Produit(string n,float p){
+            nom = n;
+            prixHT = p;
+        }
+
+        float getPrixTTC() {
+            return prixHT *(1+ tauxTVA/100);
+        }
+
+        static void modifierTVA(float nouveauTaux){
+            tauxTVA = nouveauTaux;
+        }
+
+        void afficher()  {
+            cout << "Produit : " << nom 
+            << " | Prix HT : " << prixHT 
+            << " | Prix TTC : " << getPrixTTC() << " DH" << endl;
+    }
+    
+    ~Produit(){
+        cout<<"fin"<<endl;
+    }
+};
+
+float Produit::tauxTVA = 20.0;
+
+int main()
+{
+    Produit p1("banana",15);
+    Produit p2("chargeur",120);
+    Produit p3("bescuit",2);
+    Produit p4("jus",5);
+    Produit p5("pomme de terre",7);
+
+    p1.afficher();
+    p2.afficher();
+    p3.afficher();
+    p4.afficher();
+    p5.afficher();
+
+    cout<<endl;
+
+    Produit::modifierTVA(25.0);
+
+    p1.afficher();
+    p2.afficher();
+    p3.afficher();
+    p4.afficher();
+    p5.afficher();
+
+    return 0;
+}
